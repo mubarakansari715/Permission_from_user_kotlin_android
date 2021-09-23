e89dd53bdc84fba803c8111ac32953bd4e51f128
Mubarak <mubarakansari715@gmail.com>
2021-09-23 16:20:40 +0530

    Update

--- app/src/main/java/com/example/android_permission/MainActivity.kt
+++ app/src/main/java/com/example/android_permission/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.android_permission
 
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.pm.PackageManager
-import android.graphics.Camera
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import android.Manifest.permission.CAMERA
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     private val CAMERA_PERMISSION_CODE = 123
     private val STORAGE_PERMISSION_CODE = 112
+    private val CONTACT_PERMISSION_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,50 +20,106 @@ class MainActivity : AppCompatActivity() {
 
 
         btn_camera.setOnClickListener {
-            CheckPermissions(CAMERA,CAMERA_PERMISSION_CODE)
+            //enable to Permissions
+            enableSelfPermission(CAMERA, CAMERA_PERMISSION_CODE)
         }
         btn_storege.setOnClickListener {
-            CheckPermissions(READ_EXTERNAL_STORAGE,STORAGE_PERMISSION_CODE)
+            //enable to Permissions
+            enableSelfPermission(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                STORAGE_PERMISSION_CODE
+            )
+        }
+        btn_contact.setOnClickListener {
+            //enable to Permissions
+            enableSelfPermission(android.Manifest.permission.READ_CONTACTS, CONTACT_PERMISSION_CODE)
         }
     }
 
-    //global
-    private fun CheckPermissions(permission: String, requestCode: Int) {
-        if (ContextCompat.checkSelfPermission(this,
-                permission) == PackageManager.PERMISSION_DENIED
+    private fun enableSelfPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-
 
         } else {
-            Toast.makeText(this, "Permission Granted Already", Toast.LENGTH_SHORT).show()
+            //Ask for permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    permission
+                )
+            ) {
+                //We need to show user a dialog for displaying why the permission is needed and then ask for the permission...
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permission),
+                    requestCode
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permission),
+                    requestCode
+                )
+            }
         }
     }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray,
+        permissions: Array<String>,
+        grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_CODE) {
+        if (requestCode == CONTACT_PERMISSION_CODE) {
+
+            //contact
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, " Camera Permission Granted", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+                //We have the permission
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_CONTACTS
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_CONTACTS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
             }
 
-        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            //camera
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+                //We have the permission
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
             }
 
+            //READ_EXTERNAL_STORAGE
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //We have the permission
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+            }
         }
     }
-
-
 }
 
 
